3578417c83b6eafb1c19d121f702880c50218cc9
Shamsiddin <sh.toshmirzaev@gmail.com>
2019-11-02 20:03:38 +0500

    Enabled Pretty URL

--- common/config/main.php
+++ common/config/main.php
@@ -1,13 +1,45 @@
 <?php
+
+
+$params = array_merge(
+    require(__DIR__ . '/../../common/config/params.php'),
+    require(__DIR__ . '/../../common/config/params-local.php'),
+    require(__DIR__ . '/params.php'),
+    require(__DIR__ . '/params-local.php')
+);
+
+
 return [
     'aliases' => [
         '@bower' => '@vendor/bower-asset',
         '@npm'   => '@vendor/npm-asset',
+        '@url'   => $params['siteUrl'],
     ],
     'vendorPath' => dirname(dirname(__DIR__)) . '/vendor',
     'components' => [
         'cache' => [
             'class' => 'yii\caching\FileCache',
         ],
+        'urlManager' => [
+            'enablePrettyUrl' => true,
+//            'showScriptName' => false,
+            'enableStrictParsing' => false,
+        ],
+
+
+        'i18n' => [
+            'translations' => [
+                'app' => [
+                    'class' => 'yii\i18n\PhpMessageSource',
+                    'basePath' => '@common/messages',
+                    'sourceLanguage' => 'en',
+                    'fileMap' => [
+                        'app' => 'app.php',
+                        'app/error' => 'error.php',
+                    ],
+                ],
+            ]
+        ],
+
     ],
 ];
