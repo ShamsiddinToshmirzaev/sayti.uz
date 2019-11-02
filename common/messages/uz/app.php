6d4d4f7442e64063a3f49bb267be64e5d4415f41
Shamsiddin <sh.toshmirzaev@gmail.com>
2019-11-02 20:04:47 +0500

    InterNationalization

---
+++ common/messages/uz/app.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    // 'Submit' => 'Отправить',
+    'Login' => 'Kirish',
+    'Logout' => 'Chiqish',
+    'Welcome' => 'Hush kelibsiz',
+    'Users' => 'Foydalanuvchilar',
+    'Email or Phone' => 'Email yoki Telefon',
+];
